c142ecdaea20c6c5d2ff2d7f573e92903c5a00e9
ElizPN <pankevych.liza@gmail.com>
2022-12-27 15:15:36 +0100

    add input value checking

--- app.js
+++ app.js
@@ -13,18 +13,37 @@ form.addEventListener("submit", (e) => {
 
   let inputValue = input.value;
 
+  // Check if there's already a city that matches the search criteria
   const listItemsArray = Array.from(list.querySelectorAll(".cities li"));
-  console.log(listItemsArray);
 
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter((el) => {
+      let content = "";
+      let cityName = el.querySelector(".city_name").textContent.toLowerCase();
+      let cityCountry = el
+        .querySelector(".city_country")
+        .textContent.toLowerCase();
 
-if (listItemsArray.length > 0) {
-  const filteredArray = listItemsArray.filter( el => {
-    let content = ""
-    let cityName = el.querySelector(".city_name").textContent.toLowerCase()
-    let cityCountry = el.querySelector(".city_country").textContent.toLowerCase()
+      // Check for the <city,country> format
+      if (inputValue.includes(",")) {
+        if (inputValue.split(",")[1].length > 2) {
+          inputValue = inputValue.split(",")[0];
+          console.log(inputValue);
+
+          // Get the content from the existing city
+          content = cityName;
+        } else {
+          // Country code is valid so keep both city and country
+          content = `${cityName},${cityCountry}`;
+          console.log(content);
+        }
+      } else {
+        // Only the <city> format is used
+        content = cityName;
+      }
+      // Return whether or not the existing content matches the form input value
+      return content == inputValue.toLowerCase();
+    });
+    console.log(filteredArray);
   }
-
-  )
-}
-
 });
